81135c9ef6e09b06592e2f1ff8faa576d990885a
Valentin Tazhetdinov <valentyntm@gmail.com>
2025-02-10 14:50:12 +0300

    Подключит store в index.html.

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,12 @@
+import { store } from './store';
+import { Provider } from 'react-redux';
+import { fetchQuestsAction, checkAuthAction } from './store/api-actions/api-actions';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app';
+
+store.dispatch(fetchQuestsAction());
+store.dispatch(checkAuthAction());
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -8,6 +14,8 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
