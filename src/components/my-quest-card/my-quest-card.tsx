e23addabbebb3e2dc11253e768f3757075f9a5ca
Valentin Tazhetdinov <valentyntm@gmail.com>
2025-02-11 23:02:46 +0300

    Добавит корректные ссылки из своих квестов.

--- src/components/my-quest-card/my-quest-card.tsx
+++ src/components/my-quest-card/my-quest-card.tsx
@@ -1,7 +1,6 @@
 import { Link } from 'react-router-dom';
-import { Path } from '../../const';
 import { ReservedQuest } from '../../types/quest';
-import { getDateLocalizedName } from '../../utils/utils';
+import { getDateLocalizedName, getQuestUrlById } from '../../utils/utils';
 import { useAppDispatch } from '../../hooks';
 import { deleteReservedQuest } from '../../store/api-actions/api-actions';
 
@@ -24,7 +23,7 @@ export default function MyQuestCard ({reservedQuest}: MyQuestCardProps): JSX.Ele
         </picture>
       </div>
       <div className="quest-card__content">
-        <div className="quest-card__info-wrapper"><Link className="quest-card__link" to={Path.Quest}>{reservedQuest.quest.title}</Link><span className="quest-card__info">[{getDateLocalizedName(reservedQuest.date)},&nbsp;{reservedQuest.time}. {reservedQuest.location.address}]</span>
+        <div className="quest-card__info-wrapper"><Link className="quest-card__link" to={getQuestUrlById(reservedQuest.quest.id)}>{reservedQuest.quest.title}</Link><span className="quest-card__info">[{getDateLocalizedName(reservedQuest.date)},&nbsp;{reservedQuest.time}. {reservedQuest.location.address}]</span>
         </div>
         <ul className="tags quest-card__tags">
           <li className="tags__item">
